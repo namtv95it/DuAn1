b816e1395f4b633f2aa7973b6fd65f5660106e8b
tavannam95 <111723138+namtv95it@users.noreply.github.com>
2023-10-10 16:46:40 +0700

    page

--- primereact-fe/app/(main)/uikit/account/page.tsx
+++ primereact-fe/app/(main)/uikit/account/page.tsx
@@ -1,7 +1,35 @@
-import React from 'react'
-
+'use client';
+import React, { useEffect, useState } from 'react';
+import { ProductService } from '../../service/ProductService';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+interface Product {
+  id: string;
+  code: string;
+  name: string;
+  description: string;
+  image: string;
+  price: number;
+  category: string;
+  quantity: number;
+  inventoryStatus: string;
+  rating: number;
+}
 export default function page() {
-  return (
-    <div>page account</div>
-  )
+  const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+        ProductService.getProductsMini().then(data => setProducts(data));
+    }, []);
+
+    return (
+        <div className="card">
+            <DataTable showGridlines value={products} tableStyle={{ minWidth: '50rem' }}>
+                <Column field="code" header="Code"></Column>
+                <Column field="name" header="Name"></Column>
+                <Column field="category" header="Category"></Column>
+                <Column field="quantity" header="Quantity"></Column>
+            </DataTable>
+        </div>
+    );
 }
