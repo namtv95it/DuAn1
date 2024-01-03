fd9c9be06a19524c0ea718372f860ee50006885a
tavannam95 <111723138+namtv95it@users.noreply.github.com>
2024-01-03 15:03:16 +0700

    basic materio

--- typescript-version/src/@core/layouts/components/vertical/navigation/VerticalNavHeader.tsx
+++ typescript-version/src/@core/layouts/components/vertical/navigation/VerticalNavHeader.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link'
 
 // ** MUI Imports
 import Box, { BoxProps } from '@mui/material/Box'
-import { styled, useTheme } from '@mui/material/styles'
+import { styled } from '@mui/material/styles'
 import Typography, { TypographyProps } from '@mui/material/Typography'
 
 // ** Type Import
@@ -52,7 +52,7 @@ const VerticalNavHeader = (props: Props) => {
   const { verticalNavMenuBranding: userVerticalNavMenuBranding } = props
 
   // ** Hooks
-  const theme = useTheme()
+  // const theme = useTheme()
 
   return (
     <MenuHeaderWrapper className='nav-header' sx={{ pl: 6 }}>
@@ -61,7 +61,8 @@ const VerticalNavHeader = (props: Props) => {
       ) : (
         <Link href='/' passHref>
           <StyledLink>
-            <svg
+            <img src="images/logoN.png" width={'15%'} alt="" />
+            {/* <svg
               width={30}
               height={25}
               version='1.1'
@@ -119,7 +120,7 @@ const VerticalNavHeader = (props: Props) => {
                   </g>
                 </g>
               </g>
-            </svg>
+            </svg> */}
             <HeaderTitle variant='h6' sx={{ ml: 3 }}>
               {themeConfig.templateName}
             </HeaderTitle>
